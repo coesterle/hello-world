b1a8f6ab68091396a958a5afba8b2026383cd821
coesterle <christian.oesterle@gmx.at>
2018-11-10 15:50:14 +0100

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //another local change (not in remote repo)
+	//this is just a test if the diff tool works
     }
-}
+
